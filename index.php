a571faca998fe794c0fb5d58efba02cbab671ca4
harihari1009 <wellkazuya@gmail.com>
2019-07-24 16:38:54 +0900

    add

--- index.php
+++ index.php
@@ -22,12 +22,12 @@ require "function.php";
   <form action="" method="POST">
     <input type="text" name="username" placeholder="user name" value="<?php echo htmlspecialchars($username, ENT_QUOTES, 'UTF-8'); ?>">
     <input type="submit" value="Check!">
-    <?php  dbConnect();
+    <?php  $dbn = dbConnect();
     // SQL作成
     $sql = "SELECT * FROM user";
 
 	// SQL実行
-	$res = $dbh->query($sql);
+	$res = $dbn->query($sql);
 
 	// 取得したデータを出力
 	foreach( $res as $value ) {
