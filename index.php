9e771a25cd31270debaecdada799335af3cc23fb
harihari1009 <wellkazuya@gmail.com>
2019-07-24 15:46:24 +0900

    add

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php
 
-// コメント
-# コメント
-/*
-コメント
-コメント
-コメント
-*/
-
-echo "hello from the TOP!";
+$username = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $username = $_POST['username'];
+  $err = false;
+  if (strlen($username) > 8) {
+    $err = true;
+  }
+}
 
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Check username</title>
+</head>
 <body>
-  <p>Hello World <?php echo " from PHP"; ?></p>
+  <form action="" method="POST">
+    <input type="text" name="username" placeholder="user name" value="<?php echo htmlspecialchars($username, ENT_QUOTES, 'UTF-8'); ?>">
+    <input type="submit" value="Check!">
+    <?php if ($err) { echo "Too long!"; } ?>
+  </form>
 </body>
 </html>
